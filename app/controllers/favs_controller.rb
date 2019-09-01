184a5fde2119cea8e807d7d24a30c0032ef62f69
kazuhiro.kamei <kameigitkazuhiro@gmail.com>
2019-09-01 13:07:55 +0000

    likeボタン設置（動作確認済）ナビバーにlikesメニュー

--- app/controllers/favs_controller.rb
+++ app/controllers/favs_controller.rb
@@ -5,13 +5,13 @@ class FavsController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.fav(micropost)
     flash[:success] = "投稿をお気に入りにしました。"
-    redirect_to micropost
+    redirect_to :root
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfav(micropost)
     flash[:success] = "投稿のお気に入りを解除しました。"
-    redirect_to micropost
+    redirect_to :root
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 2019_08_31_112825) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favs", "microposts"
   add_foreign_key "favs", "users"
-  add_foreign_key "favs", "users", column: "micropost_id"
   add_foreign_key "microposts", "users"
   add_foreign_key "relationships", "users"
   add_foreign_key "relationships", "users", column: "follow_id"

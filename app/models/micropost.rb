3b6845f03312f147a47179d0993db397d4036cd1
kazuhiro.kamei <kameigitkazuhiro@gmail.com>
2019-09-01 14:10:45 +0000

    dependent: :destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,6 @@ class Micropost < ApplicationRecord
   
   validates :content, presence: true, length:{maximum:255}
   
-  has_many :reverses_of_fav, class_name: "Fav", foreign_key: "micropost_id"
-  has_many :fav_users, through: :reverses_of_fav, source: :user
+  has_many :reverses_of_fav, class_name: "Fav", foreign_key: "micropost_id",dependent: :destroy
+  has_many :fav_users, through: :reverses_of_fav, source: :user,dependent: :destroy
 end
